f33be18a87dd3a28a8d586878509254e7f77bf8c
aayambhatt <aayambhatt23@gmail.com>
2024-10-13 18:53:42 +0530

    file added

---
+++ Strings/isSubsequence.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i =0, j =0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+    if(i>=s.size()){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+        
+    }
+};
+
+
+int main()
+{
+ return 0;
+}
